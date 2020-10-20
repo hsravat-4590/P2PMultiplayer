bc478d9d21e59439ded1dc610ca8978bf0d6e72d
Hanzalah Ravat <hanzalah.ravat@gmail.com>
2020-10-20 15:06:35 +0100

    net: define allowed network protocols

---
+++ net/Protocol.h
@@ -0,0 +1,13 @@
+////
+// Created by hanza on 17/10/2020.
+//
+//
+#ifndef P2PMULTIPLAYER_PROTOCOL_H
+#define P2PMULTIPLAYER_PROTOCOL_H
+
+enum Protocols{
+    TCP,
+    UDP
+};
+
+#endif //P2PMULTIPLAYER_PROTOCOL_H
